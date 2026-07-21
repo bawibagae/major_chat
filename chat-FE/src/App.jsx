81ed203c91902e317fb968e7704a048085077320
최재우 <gggghhjjp@gmail.com>
2026-07-22 08:37:00 +0900

    eni

--- chat-FE/src/App.jsx
+++ chat-FE/src/App.jsx
@@ -9,7 +9,7 @@ import Profile from "./profile/Profile";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/chat" element={<ChatPage />} />
